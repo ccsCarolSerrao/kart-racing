47cd2d09a7bab8fdcea1e5401886945df19e1753
Carol Serrão <carolina.serrao@bodytech.com.br>
2019-08-21 00:47:20 -0300

    file path

--- src/middlewares/upload-file.middleware.ts
+++ src/middlewares/upload-file.middleware.ts
@@ -3,7 +3,7 @@ import multer from 'multer'
 
 export namespace UploadFileMiddleware {
     export function uploadFile(req: Request, res: Response, next: NextFunction): void {
-        const upload: RequestHandler = multer().single('raceLog')
+        const upload: RequestHandler = multer({ dest: 'upload/' }).single('raceLog')
 
         upload(req, res, (error) => {
             if (error) { throw error }
